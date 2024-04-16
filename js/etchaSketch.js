7f85ceec81d5d773d4b574609894a63f9ac1b25a
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-16 17:36:00 +0100

    Add mouse event listeners when clicked and dragged to fill the grid with background colour

--- js/etchaSketch.js
+++ js/etchaSketch.js
@@ -1,5 +1,7 @@
 const container = document.querySelector(".squaresContainer");
+let isMouseDown = false;
 
+// Creates 16 by 16 div grid
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
@@ -7,3 +9,28 @@ for (let i = 0; i < 16; i++) {
         container.appendChild(square);
     }
 }
+
+// Event listeners for when mouse button is clicked and dragged
+container.addEventListener("mousedown", () => {
+    isMouseDown = true;
+});
+
+container.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
+
+container.addEventListener("mousemove", (event) => {
+    if (isMouseDown) {
+        if (event.target.classList.contains("square")) {
+            event.target.style.backgroundColor = "black";
+        }
+    }
+});
+
+container.addEventListener("click", (event) => {
+    if (!isMouseDown) {
+        if (event.target.classList.contains("square")) {
+            event.target.style.backgroundColor = "black";
+        }
+    }
+});
